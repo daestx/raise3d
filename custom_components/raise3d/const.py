173a1bfcdb0c7bccc77436292b2229d7a2f9891b
daest <steffen@daehmcke.de>
2024-06-02 14:48:56 +0200

    added some sensors

--- custom_components/raise3d/const.py
+++ custom_components/raise3d/const.py
@@ -1,6 +1,16 @@
 """Constants for Raise3D printer configuration."""
 from logging import Logger, getLogger
 
+from homeassistant.const import (
+    UnitOfTime,
+    PERCENTAGE,
+    UnitOfTemperature,
+    UnitOfMass,
+    UnitOfPower,
+    UnitOfEnergy,
+)
+
+
 LOGGER: Logger = getLogger(__package__)
 
 NAME = "Raise3D"
@@ -17,5 +27,27 @@ CONF_NAME = "Raise3D"
 CONF_SCAN_INTERVAL = 10
 CONF_PORT = 10800
 CONF_IP = "IP"
+
+ATTR_STATUS_DESCRIPTION = "status_description"
+ATTR_MANUFACTURER = "Raise 3D"
+ATTR_MODEL = "Pro2"
+
+SYSTEM_SENSOR_TYPES = {
+    "L_status": [
+        "Outside Temperature",
+        "L_status",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_errors": [
+        "System Errors",
+        "L_errors",
+        None,
+        "mdi:alert-circle",
+    ],
+}
+
+
+
 
 
